e39baeb22c46fa9afff51c62ac1fb761632eeaa3
DominicTjiptono <37432426+DominicTjiptono@users.noreply.github.com>
2019-09-07 21:49:06 +1200

    Add files via upload

--- Heroes of Dicury Planet/heroes_of_dicury_planet.py
+++ Heroes of Dicury Planet/heroes_of_dicury_planet.py
@@ -1,7 +1,7 @@
 # The names of the monsters in this game are randomly generated using the random generator in the link below.
 # https://www.fantasynamegenerators.com/
 
-# Game version: pre-release 3 (Command Line Interface Version)
+# Game version: pre-release 4 (Command Line Interface Version)
 
 
 import sys
@@ -32,7 +32,7 @@ today = date.today()
 
 
 def triangulation(n: int) -> int:
-    return int(n * (n - 1) / 2)
+    return int(n * (n + 1) / 2)
 
 
 def all_died(team):
@@ -81,6 +81,10 @@ class Action:
         else:
             return False
 
+    def clone(self):
+        # type: () -> Action
+        return copy.deepcopy(self)
+
 
 class Hero:
     """
@@ -92,27 +96,52 @@ class Hero:
     MIN_CRIT_RATE: float = 0.15
     MAX_CRIT_RATE: float = 1
     MIN_CRIT_DAMAGE: float = 2
+    MIN_RESISTANCE: float = 0
+    MAX_RESISTANCE: float = 1
+    MIN_ACCURACY: float = 0
+    MAX_ACCURACY: float = 1
+    MIN_BUFFS: int = 0
+    MAX_BUFFS: int = 10
+    MIN_DEBUFFS: int = 0
+    MAX_DEBUFFS: int = 10
 
     def __init__(self, name, rating, max_hp, attack_power, defense, skills):
         # type: (str, int, Decimal, Decimal, Decimal, list) -> None
         self.name: str = name
         self.rating: int = rating if self.MIN_RATING <= rating <= self.MAX_RATING else 1
         self.level: int = 1
-        self.max_level: int = triangulation(self.level)
+        self.max_level: int = triangulation(self.rating) * 10
         self.curr_hp: Decimal = max_hp
         self.max_hp: Decimal = max_hp
         self.attack_power: Decimal = attack_power
         self.defense: Decimal = defense
         self.crit_rate: float = self.MIN_CRIT_RATE
         self.crit_damage: float = self.MIN_CRIT_DAMAGE
+        self.resistance: float = self.MIN_RESISTANCE
+        self.accuracy: float = self.MIN_ACCURACY
         self.skills: list = skills
+        self.buffs: list = []
+        self.debuffs: list = []
         self.is_alive = True
         self.corresponding_team: Team or None = None  # initial value
+
+    def clone(self):
+        # type: () -> Hero
+        return copy.deepcopy(self)
 
     def get_is_alive(self):
         # type: () -> bool
         self.is_alive = self.curr_hp > 0
         return self.is_alive
+
+    def resistance_accuracy_rule(self, other):
+        # type: (Hero) -> float
+        if self.accuracy >= other.resistance:
+            return 0.15
+        elif self.accuracy < other.resistance and other.resistance - self.accuracy <= 0.15:
+            return 0.15
+        else:
+            return other.resistance - self.accuracy
 
     def to_string(self) -> str:
         res: str = ""  # initial value
@@ -125,12 +154,78 @@ class Hero:
         res += "Defense: " + str(self.defense) + "\n"
         res += "Crit rate: " + str(100*self.crit_rate) + "%\n"
         res += "Crit damage: " + str(100 * self.crit_damage) + "%\n"
+        res += "Resistance: " + str(100 * self.resistance) + "%\n"
+        res += "Accuracy: " + str(100 * self.accuracy) + "%\n"
         res += "Below is a list of skills this hero has.\n"
         for skill in self.skills:
             res += str(skill.to_string()) + "\n"
 
+        res += "Below is a list of buffs that this hero has.\n"
+        for buff in self.buffs:
+            res += str(buff.to_string()) + "\n"
+
+        res += "Below is a list of debuffs that this hero has.\n"
+        for debuff in self.debuffs:
+            res += str(debuff.to_string()) + "\n"
+
         res += "Is it alive? " + str(self.get_is_alive()) + "\n"
         return res
+
+    def add_buff(self, buff):
+        # type: (Buff) -> bool
+        if len(self.buffs) < self.MAX_BUFFS:
+            buff_name_list: list = [curr_buff.name for curr_buff in self.buffs]
+            if buff.name not in buff_name_list or buff.is_stackable:
+                self.buffs.append(buff)
+                self.attack_power *= Decimal(1 + buff.attack_percentage_up/100)
+                self.defense *= Decimal(1 + buff.defense_percentage_up/100)
+                return True
+            return False
+        return False
+
+    def remove_buff(self, buff):
+        # type: (Buff) -> bool
+        if buff in self.buffs:
+            self.buffs.remove(buff)
+            self.attack_power /= Decimal(1 + buff.attack_percentage_up / 100)
+            self.defense /= Decimal(1 + buff.defense_percentage_up / 100)
+            return True
+        return False
+
+    def add_debuff(self, debuff):
+        # type: (Debuff) -> bool
+        if len(self.debuffs) < self.MAX_DEBUFFS:
+            debuff_name_list: list = [curr_debuff.name for curr_debuff in self.debuffs]
+            if debuff.name not in debuff_name_list or debuff.is_stackable:
+                self.debuffs.append(debuff)
+                self.attack_power *= Decimal(1 - debuff.attack_percentage_down / 100)
+                self.defense *= Decimal(1 - debuff.defense_percentage_down / 100)
+                return True
+            return False
+        return False
+
+    def remove_debuff(self, debuff):
+        # type: (Debuff) -> bool
+        if debuff in self.debuffs:
+            self.debuffs.remove(debuff)
+            self.attack_power /= Decimal(1 - debuff.attack_percentage_down / 100)
+            self.defense /= Decimal(1 - debuff.defense_percentage_down / 100)
+            return True
+        return False
+
+    def reduce_buffs_count(self):
+        # type: () -> None
+        for buff in self.buffs:
+            buff.duration -= 1
+            if buff.duration == 0:
+                self.remove_buff(buff)
+
+    def reduce_debuffs_count(self):
+        # type: () -> None
+        for debuff in self.debuffs:
+            debuff.duration -= 1
+            if debuff.duration == 0:
+                self.remove_debuff(debuff)
 
     def attack(self, other):
         # type: (Hero) -> str
@@ -186,13 +281,16 @@ class Skill:
     This class contains attributes of skills that a hero has.
     """
 
-    def __init__(self, name, does_heal, does_attack, heal_amount, damage_multiplier):
-        # type: (str, bool, bool, Decimal, float) -> None
+    def __init__(self, name, does_heal, does_attack, heal_amount, damage_multiplier, buffs_to_allies,
+                 debuffs_to_enemies):
+        # type: (str, bool, bool, Decimal, float, list, list) -> None
         self.name: str = name
         self.does_heal: bool = does_heal
         self.does_attack: bool = does_attack
         self.heal_amount: Decimal = heal_amount if self.does_heal else 0
         self.damage_multiplier: float = damage_multiplier if self.does_attack else 0
+        self.buffs_to_allies: list = buffs_to_allies
+        self.debuffs_to_enemies: list = debuffs_to_enemies
 
     def to_string(self) -> str:
         res: str = ""  # initial value
@@ -201,7 +299,77 @@ class Skill:
         res += "Does it attack? " + str(self.does_attack) + "\n"
         res += "Heal amount: " + str(self.heal_amount) + "\n"
         res += "Damage multiplier: " + str(self.damage_multiplier) + "\n"
+        res += "Below is a list of buffs that the skill gives to allies.\n"
+        for buff in self.buffs_to_allies:
+            res += str(buff.to_string()) + "\n"
+
+        res += "Below is a list of debuffs that the skill gives to enemies.\n"
+        for debuff in self.debuffs_to_enemies:
+            res += str(debuff.to_string()) + "\n"
+
         return res
+
+    def clone(self):
+        # type: () -> Skill
+        return copy.deepcopy(self)
+
+
+class Buff:
+    """
+    This class contains attributes of beneficial effects.
+    """
+    POSSIBLE_NAMES = ["ATTACK_UP", "DEFENSE_UP"]
+
+    def __init__(self, name, duration):
+        # type: (str, int) -> None
+        self.name: str or None = name if name in self.POSSIBLE_NAMES else None
+        self.attack_percentage_up: float = 50 if self.name == "ATTACK_UP" else 0
+        self.defense_percentage_up: float = 50 if self.name == "DEFENSE_UP" else 0
+        self.duration: int = duration
+        self.is_stackable: bool = False
+
+    def to_string(self):
+        # type: () -> str
+        res: str = ""  # initial value
+        res += "Name: " + str(self.name) + "\n"
+        res += "Attack percentage up: " + str(self.attack_percentage_up) + "%\n"
+        res += "Defense percentage up: " + str(self.defense_percentage_up) + "%\n"
+        res += "Duration: " + str(self.duration) + " turns\n"
+        res += "Is it stackable? " + str(self.is_stackable) + "\n"
+        return res
+
+    def clone(self):
+        # type: () -> Buff
+        return copy.deepcopy(self)
+
+
+class Debuff:
+    """
+        This class contains attributes of harmful effects.
+        """
+    POSSIBLE_NAMES = ["ATTACK_DOWN", "DEFENSE_DOWN"]
+
+    def __init__(self, name, duration):
+        # type: (str, int) -> None
+        self.name: str or None = name if name in self.POSSIBLE_NAMES else None
+        self.attack_percentage_down: float = 50 if self.name == "ATTACK_DOWN" else 0
+        self.defense_percentage_down: float = 50 if self.name == "DEFENSE_DOWN" else 0
+        self.duration: int = duration
+        self.is_stackable: bool = False
+
+    def to_string(self):
+        # type: () -> str
+        res: str = ""  # initial value
+        res += "Name: " + str(self.name) + "\n"
+        res += "Attack percentage down: " + str(self.attack_percentage_down) + "%\n"
+        res += "Defense percentage down: " + str(self.defense_percentage_down) + "%\n"
+        res += "Duration: " + str(self.duration) + " turns\n"
+        res += "Is it stackable? " + str(self.is_stackable) + "\n"
+        return res
+
+    def clone(self):
+        # type: () -> Debuff
+        return copy.deepcopy(self)
 
 
 class Team:
@@ -228,6 +396,19 @@ class Team:
             return True
         return False
 
+    def to_string(self):
+        # type: () -> str
+        res: str = ""  # initial value
+        res += "Below is a list of heroes in the team.\n"
+        for hero in self.heroes:
+            res += str(hero.to_string()) + "\n"
+
+        return res
+
+    def clone(self):
+        # type: () -> Team
+        return copy.deepcopy(self)
+
 
 def main():
     """
@@ -236,19 +417,21 @@ def main():
     """
     heroes_list: list = [
         Hero("Hydra", 1, Decimal("5e4"), Decimal("2.5e3"), Decimal("2.47e3"),
-             [Skill("Strike", False, True, Decimal("0e0"), 3.5), Skill("Dispel", True, False, Decimal("1e4"), 0),
-              Skill("Smother", False, True, Decimal("0e0"), 8)]),
+             [Skill("Strike", False, True, Decimal("0e0"), 3.5, [], [Debuff("ATTACK_DOWN", 3)]),
+              Skill("Dispel", True, False, Decimal("1e4"), 0, [Buff("ATTACK_UP", 2), Buff("DEFENSE_UP", 2)], []),
+              Skill("Smother", False, True, Decimal("0e0"), 8, [], [Debuff("DEFENSE_DOWN", 3)])]),
         Hero("Cygrus", 1, Decimal("4.78e4"), Decimal("2.7e3"), Decimal("2.11e3"),
-             [Skill("Strike", False, True, Decimal("0e0"), 3.5), Skill("Dispel", True, False, Decimal("1e4"), 0),
-              Skill("Smother", False, True, Decimal("0e0"), 8)])
+             [Skill("Strike", False, True, Decimal("0e0"), 3.5, [], [Debuff("ATTACK_DOWN", 3)]),
+              Skill("Dispel", True, False, Decimal("1e4"), 0, [Buff("ATTACK_UP", 2), Buff("DEFENSE_UP", 2)], []),
+              Skill("Smother", False, True, Decimal("0e0"), 8, [], [Debuff("DEFENSE_DOWN", 3)])])
     ]
 
     player_team: Team = Team()
     enemy_team: Team = Team()
 
     for i in range(5):
-        player_team.add_hero_(heroes_list[random.randint(0, len(heroes_list) - 1)])
-        enemy_team.add_hero_(heroes_list[random.randint(0, len(heroes_list) - 1)])
+        player_team.add_hero_(heroes_list[random.randint(0, len(heroes_list) - 1)].clone())
+        enemy_team.add_hero_(heroes_list[random.randint(0, len(heroes_list) - 1)].clone())
 
     for hero in player_team.heroes:
         hero.corresponding_team = player_team
@@ -264,14 +447,8 @@ def main():
 
     while option != 2:
         if option == 1:
-            print("Battle starts. Below is a list of your heroes.\n")
-            for hero in player_team.heroes:
-                print(hero.to_string())
-
-            print("Below is a list of your enemy's heroes.\n")
-            for hero in enemy_team.heroes:
-                print(hero.to_string())
-
+            print(player_team.to_string())
+            print(enemy_team.to_string())
             turn: int = 0  # initial value
             while not all_died(player_team) and not all_died(enemy_team):
                 turn += 1
@@ -286,6 +463,8 @@ def main():
 
                         moving_hero = player_team.heroes[hero_index]
 
+                    moving_hero.reduce_buffs_count()
+                    moving_hero.reduce_debuffs_count()
                     print("Type in NORMAL ATTACK, NORMAL HEAL, or USE SKILL")
                     action_name: str = input("What do you want to do? ")
                     target: Hero or None = None  # initial value
@@ -335,6 +514,9 @@ def main():
                                                                     "Please enter index of enemy's hero: "))
 
                             target = enemy_team.heroes[enemy_target_index]
+                            for debuff in skill_to_use.debuffs_to_enemies:
+                                if random.random() > moving_hero.resistance_accuracy_rule(target):
+                                    target.add_debuff(debuff.clone())
 
                         elif skill_to_use.does_heal:
                             ally_target_index: int = int(input("Please enter index of ally's hero: "))
@@ -347,18 +529,16 @@ def main():
                                                                    "Please enter index of ally's hero: "))
 
                             target = player_team.heroes[ally_target_index]
+                            for buff in skill_to_use.buffs_to_allies:
+                                target.add_buff(buff.clone())
 
                     action: Action = Action(moving_hero, action_name, target, skill_to_use) \
                         if skill_to_use is not None else Action(moving_hero, action_name, target)
                     action.execute()
-
                     print("Current status of player's heroes in the battle are as below.\n")
-                    for hero in player_team.heroes:
-                        print(hero.to_string())
-
+                    print(player_team.to_string())
                     print("Current status of enemy's heroes in the battle are as below.\n")
-                    for hero in enemy_team.heroes:
-                        print(hero.to_string())
+                    print(enemy_team.to_string())
 
                     if all_died(player_team):
                         print("You lose!")
@@ -377,6 +557,8 @@ def main():
 
                         moving_hero = enemy_team.heroes[hero_index]
 
+                    moving_hero.reduce_buffs_count()
+                    moving_hero.reduce_debuffs_count()
                     action_name: str = Action.POSSIBLE_NAMES[random.randint(0, len(Action.POSSIBLE_NAMES) - 1)]
                     skill_to_use: Skill or None = None  # initial value
                     target: Hero or None = None  # initial value
@@ -398,22 +580,25 @@ def main():
                             while not target.get_is_alive():
                                 target = player_team.heroes[random.randint(0, len(player_team.heroes) - 1)]
 
+                            for debuff in skill_to_use.debuffs_to_enemies:
+                                if random.random() > moving_hero.resistance_accuracy_rule(target):
+                                    target.add_debuff(debuff.clone())
+
                         elif skill_to_use.does_heal:
                             target = enemy_team.heroes[random.randint(0, len(enemy_team.heroes) - 1)]
                             while not target.get_is_alive():
                                 target = enemy_team.heroes[random.randint(0, len(enemy_team.heroes) - 1)]
 
+                            for buff in skill_to_use.buffs_to_allies:
+                                target.add_buff(buff.clone())
+
                     action: Action = Action(moving_hero, action_name, target, skill_to_use) \
                         if skill_to_use is not None else Action(moving_hero, action_name, target)
                     action.execute()
-
                     print("Current status of player's heroes in the battle are as below.\n")
-                    for hero in player_team.heroes:
-                        print(hero.to_string())
-
+                    print(player_team.to_string())
                     print("Current status of enemy's heroes in the battle are as below.\n")
-                    for hero in enemy_team.heroes:
-                        print(hero.to_string())
+                    print(enemy_team.to_string())
 
                     if all_died(player_team):
                         print("You lose!")
